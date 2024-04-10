7344a9f7811a3b9526f1ec7260652cf8bbc4ac30
Oleg Ponikarenko <olegsun2001@gmail.com>
2024-04-10 05:57:16 +0300

    napisana programmaвое

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Ввод данных
+string[] initialArray = new string[] { "Привет", "мир", "!", "12345", "abc", "Разное","ку" };
+#endregion
+
+#region Подсчет количества строк, удовлетворяющих условию
+int count = 0;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+#endregion
+
+#region Формирование нового массива
+string[] resultArray = new string[count];
+int index = 0;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= 3)
+    {
+        resultArray[index++] = initialArray[i];
+    }
+}
+#endregion
+
+#region Вывод результата
+Console.WriteLine("Исходный массив:");
+foreach (string str in initialArray)
+{
+    Console.WriteLine(str);
+}
+
+Console.WriteLine("\nНовый массив:");
+foreach (string str in resultArray)
+{
+    Console.WriteLine(str);
+}
+#endregion
